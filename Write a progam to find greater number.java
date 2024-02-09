a0c7994f83be43d26a4d28111e302f0bab26eda0
Logesh <84928658+IVAR23-NITRO@users.noreply.github.com>
2024-02-09 14:51:20 +0530

    Create Write a progam to find greater number.java

---
+++ Write a progam to find greater number.java
@@ -0,0 +1,9 @@
+Scanner in=new Scanner(System.in);
+		int userInput1=in.nextInt();
+		int userInput2=in.nextInt();
+		if(userInput1>userInput2) {
+			System.out.println(userInput1);
+		}else{
+			System.out.println(userInput2);
+		}
+	
