95edc610298948df4bc8396f11d46b2838f5d5b4
Logesh <84928658+IVAR23-NITRO@users.noreply.github.com>
2024-02-09 14:49:01 +0530

    Create Write function to find multiplication of 2 numbers using + operator You must use minimum possible iterations..java
    multiplication without using  *

---
+++ Write function to find multiplication of 2 numbers using + operator You must use minimum possible iterations..java
@@ -0,0 +1,11 @@
+		Scanner in=new Scanner(System.in);
+		int userInput1=in.nextInt();
+		int userInput2=in.nextInt();
+		int k=0;
+		
+		for(int i =1;i<=userInput2;i++) {
+			k=userInput1+k;
+		}
+		System.out.println(k);
+	
+		
