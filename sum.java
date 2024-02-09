dab7d3e31600503ad043b26d92d474923b370039
Logesh <84928658+IVAR23-NITRO@users.noreply.github.com>
2024-02-09 14:18:12 +0530

    Create sum.java
    sum problem from heacker rank

---
+++ sum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Sum {
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int userInput1=in.nextInt();
+		int userInput2=in.nextInt();
+		int sum=userInput1+userInput2;
+		
+		
+		System.out.println(sum);
+		
+	}
+
+}
